4fd84f3649f2945c08385274b95408be6a93f50e
johnnywwy <johnnywwy@gmail.com>
2022-03-21 23:40:29 +0800

    Money.tsx 之 TagSection

--- src/App.tsx
+++ src/App.tsx
@@ -7,27 +7,34 @@ import Tags from './Views/Tags';
 import Money from './Views/Money';
 import Statistics from './Views/Statistics';
 import NoMatch from './Views/NoMatch';
+import styled from 'styled-components';
+
+const AppWrapper = styled.div`
+  color: #333
+`;
 
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route path="/tags">
-          <Tags/>
-        </Route>
-        <Route path="/money">
-          <Money/>
-        </Route>
-        <Route path="/statistics">
-          <Statistics/>
-        </Route>
-        <Redirect exact from="/" to="/money/"/>
-        <Route path="*">
-          <NoMatch/>
-        </Route>
-      </Switch>
-    </Router>
+    <AppWrapper>
+      <Router>
+        <Switch>
+          <Route path="/tags">
+            <Tags/>
+          </Route>
+          <Route path="/money">
+            <Money/>
+          </Route>
+          <Route path="/statistics">
+            <Statistics/>
+          </Route>
+          <Redirect exact from="/" to="/money/"/>
+          <Route path="*">
+            <NoMatch/>
+          </Route>
+        </Switch>
+      </Router>
+    </AppWrapper>
   );
 }
 
