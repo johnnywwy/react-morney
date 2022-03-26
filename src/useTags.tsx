68ab5797dc8e3f144d008a5ab9d97c5e19c071ca
johnnywwy <johnnywwy@gmail.com>
2022-03-26 21:59:04 +0800

    Tag.tsx 编辑功能完成

--- src/useTags.tsx
+++ src/useTags.tsx
@@ -11,8 +11,27 @@ const defaultTags = [
 const useTags = () => {//封装一个自定义hook
   const [tags, setTags] = useState<{ id: number; name: string }[]>(defaultTags);
   const findTag = (id: number) => tags.filter(tag => tag.id === id)[0];
+  const findTagIndex = (id: number) => {
+    let result = -1;
 
-  return {tags, setTags,findTag};
+    for (let i = 0; i < tags.length; i++) {
+      if (tags[0].id === id) {
+        result = i;
+        break;
+      }
+    }
+    return result;
+  };
+  const updateTag = (id: number, obj: { name: string }) => {
+    const index = findTagIndex(id);
+    console.log(index);
+    console.log(tags);
+    //深拷贝 tags 得到 tagClone
+    const tagsClone = JSON.parse(JSON.stringify(tags));
+    tagsClone.splice(index, 1, {id: id, name: obj.name});
+    setTags(tagsClone);
+  };
+  return {tags, setTags, findTag, findTagIndex, updateTag};
 };
 
 export {useTags};
