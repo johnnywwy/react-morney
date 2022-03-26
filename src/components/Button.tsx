a2bd277dcf27b1ce49a10253a743d3bfbd09d859
johnnywwy <johnnywwy@gmail.com>
2022-03-26 18:46:55 +0800

    创建单独的 Button 组件

---
+++ src/components/Button.tsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const Button = styled.button`
+  color: white;
+  background: #767676;
+  font-size: 18px;
+  padding: 8px 13px;
+  border: none;
+  border-radius: 4px;
+`;
+
+export {Button}
