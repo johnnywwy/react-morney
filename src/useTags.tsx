0d0b90287dc2ee9b23d2be70442dd7f2ca8afded
johnnywwy <johnnywwy@gmail.com>
2022-03-26 22:16:17 +0800

    删除tag

--- src/useTags.tsx
+++ src/useTags.tsx
@@ -28,7 +28,16 @@ const useTags = () => {//封装一个自定义hook
     tagsClone.splice(index, 1, {id: id, name: obj.name});
     setTags(tagsClone);
   };
-  return {tags, setTags, findTag, findTagIndex, updateTag};
+
+  const deleteTag = (id: number) => {
+    const index = findTagIndex(id);
+    const tagsClone = JSON.parse(JSON.stringify(tags));
+    tagsClone.splice(index, 1);
+    setTags(tagsClone);
+  };
+
+
+  return {tags, setTags, findTag, findTagIndex, updateTag,deleteTag};
 };
 
 export {useTags};
