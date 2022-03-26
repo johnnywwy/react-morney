4f46663bd1869ff31fc9ee81f6d86548cd9f2314
johnnywwy <johnnywwy@gmail.com>
2022-03-26 22:29:15 +0800

    重构 更新和删除tag 代码

--- src/useTags.tsx
+++ src/useTags.tsx
@@ -22,22 +22,15 @@ const useTags = () => {//封装一个自定义hook
     return result;
   };
   const updateTag = (id: number, obj: { name: string }) => {
-    const index = findTagIndex(id);
-    //深拷贝 tags 得到 tagClone
-    const tagsClone = JSON.parse(JSON.stringify(tags));
-    tagsClone.splice(index, 1, {id: id, name: obj.name});
-    setTags(tagsClone);
+    setTags(tags.map(tag => tag.id === id ? {id, name: obj.name} : tag));
   };
 
   const deleteTag = (id: number) => {
-    const index = findTagIndex(id);
-    const tagsClone = JSON.parse(JSON.stringify(tags));
-    tagsClone.splice(index, 1);
-    setTags(tagsClone);
+    setTags(tags.filter(tag => tag.id !== id));
   };
 
 
-  return {tags, setTags, findTag, findTagIndex, updateTag,deleteTag};
+  return {tags, setTags, findTag, findTagIndex, updateTag, deleteTag};
 };
 
 export {useTags};
