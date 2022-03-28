76e906529c4ff3634871d8921749f748f6992a0c
johnnywwy <johnnywwy@gmail.com>
2022-03-28 22:41:43 +0800

    设置homepage

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,6 +6,6 @@ git init &&
 git add . &&
 git commit -m "deploy" &&
 git remote add origin git@github.com:johnnywwy/react-morney-website.git &&
-git branch -M main&&
-git push -u origin main
+git branch -M main &&
+git push -u origin main -f
 cd ..
