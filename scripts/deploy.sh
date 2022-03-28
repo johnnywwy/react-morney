81f92e882f26b76c0d1e611ea9461fb88b132552
johnnywwy <johnnywwy@gmail.com>
2022-03-28 23:35:51 +0800

    update deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,7 +5,7 @@ cd build &&
 git init &&
 git add . &&
 git commit -m "deploy" &&
-git remote add origin git@github.com:johnnywwy/react-morney-website.git &&
+git remote add origin git@github.com:johnnywwy/morney-website-react.git &&
 git branch -M main &&
 git push -u origin main -f
 cd ..
